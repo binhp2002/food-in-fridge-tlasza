724f448cd86f6108535361ba52051885afdb7ca0
binhp2002 <pduong7@gatech.edu>
2020-09-12 13:55:44 -0400

    1 file modified

--- src/ListItems.js
+++ src/ListItems.js
@@ -12,7 +12,7 @@ class TodoItems extends Component {
   createTasks(item) {
 
     return <li onClick={() => this.delete(item.key)} 
-              key={item.key}>{item.text.charAt(0).toUpperCase() + item.text.slice(1)+ " - " + item.dateAdd + " \n " + item.dateBad}</li>
+              key={item.key}>{item.text.charAt(0).toUpperCase() + item.text.slice(1)+ " - " + item.dateBad}</li>
   }
   
   render() {
